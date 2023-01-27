79e61643a1df422da0a91e2fc3d20b2938a6555b
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-01-26 21:57:18 -0300

    [AD-5] Criação do script sftp

---
+++ send-sftp.py
@@ -0,0 +1,19 @@
+import argparse
+import pysftp
+
+ALEGRIA_PATH_FOLDER = "/public_html/alegriacrista"
+
+def main(params):
+    srv = pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'])
+    with srv.cd(ALEGRIA_PATH_FOLDER):
+        srv.put(params['zip_file'])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parametros para execução do dataflow stream')
+    parser.add_argument('host', help='Host para realizar a conexão.')
+    parser.add_argument('user', help='Usuário para realizar a conexão.')
+    parser.add_argument('pass', help='Senha para realizar a conexão.')
+    parser.add_argument('zip_file', help='Arquivo que deve ser transferido.')
+    params = vars(parser.parse_args())
+
+    main(params)
