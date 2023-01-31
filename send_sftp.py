38a39711d921cc52d57eac373421552cc9c1e34d
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-01-30 21:02:35 -0300

    [AD-5] Realizando comentários e testando comando

--- send_sftp.py
+++ send_sftp.py
@@ -5,11 +5,17 @@ ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html/alegriacrist
 HOST_PORT = 22
 
 def main(params):
+    # Configurações necessárias para não verificar as chaves SSH's
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
+    # Início da conexão com o servidor da CONCAFRAS
     with pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'], port=HOST_PORT, cnopts=cnopts, log="./log_script.log") as sftp:
+        # Dentro da pasta da Alegria Cristã
         with sftp.cd(ALEGRIA_PATH_FOLDER):
             sftp.put(params['zip_file'])
+            sftp.execute("ls -lah")
+        # Encerrando conexão
+        sftp.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parametros para execução do dataflow stream')
