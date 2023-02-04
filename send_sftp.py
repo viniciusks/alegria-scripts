72be43310eaa7bd900e35001691703d3a846216d
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-02-04 16:56:00 -0300

    [AD-5] Mudando parametro

--- send_sftp.py
+++ send_sftp.py
@@ -5,21 +5,7 @@ import os
 ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html/alegriacrista"
 HOST_PORT = 22
 
-def unzip_file(zip_file):
-    os.system("pwd")
-    os.system("mkdir dist")
-    os.system(f"unzip {zip_file} dist/")
-    os.system("ls -lah")
-    dirlist = os.listdir("dist/")
-    files = []
-    for i in dirlist:
-        files.append(os.path.abspath(i))
-    return files
-
-
 def main(params):
-    files = unzip_file(params['zip_file'])
-    print(files)
     # Configurações necessárias para não verificar as chaves SSH's
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
@@ -27,7 +13,7 @@ def main(params):
     with pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'], port=HOST_PORT, cnopts=cnopts, log="./log_script.log") as sftp:
         # Dentro da pasta da Alegria Cristã
         with sftp.cd(ALEGRIA_PATH_FOLDER):
-            sftp.put(params['zip_file'])
+            sftp.put(params['file_path'])
             print(sftp.listdir())
             # sftp.execute("ls -lah")
         # Encerrando conexão
@@ -39,7 +25,7 @@ if __name__ == "__main__":
     parser.add_argument('host', help='Host para realizar a conexão.')
     parser.add_argument('user', help='Usuário para realizar a conexão.')
     parser.add_argument('pass', help='Senha para realizar a conexão.')
-    parser.add_argument('zip_file', help='Arquivo que deve ser transferido.')
+    parser.add_argument('file_path', help='Arquivo que deve ser transferido.')
     params = vars(parser.parse_args())
 
     main(params)
