92bf9cae85272854f169147bbf9c84aa3ffd402d
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-03-06 20:57:08 -0300

    [AD-9] Refatorando lógica

--- send_sftp.py
+++ send_sftp.py
@@ -3,12 +3,37 @@ import pysftp
 
 ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html/alegriacrista"
 HOST_PORT = 22
-
+DATAS_TO_DEPLOY = {
+    "site": [
+        "/assets/css/pages-styles",
+        "/assets/css",
+        "/assets/img/logo",
+        "/assets/img",
+        "/assets/js",
+        "/pages",
+        "index.html"
+    ],
+    "audio-player": [
+        "index.html"
+    ]
+}
 
 def main(params):
     # Configurações necessárias para não verificar as chaves SSH's
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
+
+    data_type = "site"
+
+    for data in DATAS_TO_DEPLOY[data_type]:
+        if "/" in data:
+            print("ENTROU NA BARRA")
+            print(data)
+            break
+        elif not "/" in data:
+            print("ENTROU NO SEM BARRA")
+            print(data)
+            break
     # Início da conexão com o servidor da CONCAFRAS
     with pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'], port=HOST_PORT, cnopts=cnopts, log="./log_script.log") as sftp:
         # Dentro da pasta da Alegria Cristã
